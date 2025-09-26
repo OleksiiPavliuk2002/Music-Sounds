b3fa4b65545fa5479755c64b28d68675fcad47df
Oleksii Pavliuk <141736751+OleksiiPavliuk2002@users.noreply.github.com>
2025-09-26 13:46:19 +0300

    Create data.js

---
+++ constants/data.js
@@ -0,0 +1,22 @@
+const drumKit = {
+  bass1: require("../assets/bass.mp3"),
+  bass2: require("../assets/bass2.mp3"),
+  bass3: require("../assets/bass3.mp3"),
+  cymbal: require("../assets/cymbal.mp3"),
+  cymbal2: require("../assets/cymbal2.mp3"),
+  drumstick: require("../assets/drumstick.mp3"),
+  hihat: require("../assets/hihat.mp3"),
+  snare: require("../assets/snare.mp3"),
+  tom: require("../assets/tom.mp3"),
+};
+
+const colors = {
+  bass: "#c0392b",
+  cymbal: "#2980b9",
+  drumstick: "#34495e",
+  hihat: "#8e44ad",
+  snare: "#d35400",
+  tom: "#95a5a6",
+};
+
+export { colors, drumKit };
